ea08bb1eba78019175a4a696606a34db39e53d68
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-14 09:46:20 +0200

    Add open connection class

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const port = 3000;
 app.use(cors());
 
 class Database {
-    constructor() {
-      this.connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-      });
-    }
+  constructor() {
+    this.connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+  }
+  openConnection() {
+    this.connection.connect((error) => {
+      if (error) throw error;
+      console.log("Connected to MySQL database!");
+    });
+  }
 }
