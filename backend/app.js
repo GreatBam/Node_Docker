adf6e2ef73bebaa0cc822abea047486132ae4f01
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-14 09:44:06 +0200

    Load dotenv in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+require('dotenv').config();
 const app = express();
 const port = 3000;
 app.use(cors());
+
