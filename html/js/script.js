cf2dba912fe3a55f397d5637ad0dc7b6990afac3
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-15 19:06:41 +0200

    Add button in html test page

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/api/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('An error occurred:', error));
+fetch("http://localhost:3000/api/data")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("An error occurred:", error));
