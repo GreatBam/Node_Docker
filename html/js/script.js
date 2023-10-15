edc8f087893694e59d86df35346179dc2944c8a0
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-15 19:09:58 +0200

    Add function in script.js to test add user

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,39 @@
+const addUserButton = document.getElementById("addUserButton");
+
 fetch("http://localhost:3000/api/data")
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((error) => console.error("An error occurred:", error));
+
+async function addUser(name, lastname, dob) {
+  const url = "http://localhost:3000/api/adduser";
+
+  const userData = {
+    name: name,
+    lastname: lastname,
+    dob: dob,
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    console.log("User added successfully!", result);
+  } else {
+    console.log("Failed to add user");
+  }
+}
+
+addUserButton.onclick = (
+  name = "John",
+  lastname = "Doe",
+  dob = "1990-01-01"
+) => {
+  addUser(name, lastname, dob);
+};
