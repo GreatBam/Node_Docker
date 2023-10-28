40dd69df33442a932ea09d4751eab09b926703be
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-28 11:48:43 +0200

    Remove unused add user

--- html/js/loginController.js
+++ html/js/loginController.js
@@ -7,31 +7,6 @@ fetch("http://localhost:3000/api/data")
   .then((data) => console.log(data))
   .catch((error) => console.error("An error occurred:", error));
 
-async function addUser(name, lastname, dob) {
-  const url = "http://localhost:3000/api/adduser";
-
-  const userData = {
-    name: name,
-    lastname: lastname,
-    dob: dob,
-  };
-
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-
-  if (response.ok) {
-    const result = await response.json();
-    console.log("User added successfully!", result);
-  } else {
-    console.log("Failed to add user");
-  }
-}
-
 async function addAccount(email, password) {
   const url = "http://localhost:3000/api/signup";
 
@@ -80,13 +55,6 @@ async function Login(email, password) {
     console.log("Failed to add user");
   }
 }
-
-// addUserButton.onclick = () => {
-//   const name = document.getElementById("username").value;
-//   const lastname = document.getElementById("userlastname").value;
-//   const dob = document.getElementById("userdob").value;
-//   addUser(name, lastname, dob);
-// };
 
 signupButton.onclick = () => {
   const email = document.getElementById("email").value;
