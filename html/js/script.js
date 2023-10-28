1fcd4760218dc24c85604d84285d627bc7f6b6a5
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-28 09:02:58 +0200

    Adapt js function for database request

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,5 @@
 const addUserButton = document.getElementById("addUserButton");
+const siginButton = document.getElementById("siginButton");
 
 fetch("http://localhost:3000/api/data")
   .then((response) => response.json())
@@ -30,9 +31,34 @@ async function addUser(name, lastname, dob) {
   }
 }
 
+async function addUser(mail, password) {
+  const url = "http://localhost:3000/api/signin";
+
+  const userData = {
+    mail: mail,
+    password: password,
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    console.log("User added successfully!", result);
+  } else {
+    console.log("Failed to add user");
+  }
+}
+
 addUserButton.onclick = () => {
   const name = document.getElementById("username").value;
   const lastname = document.getElementById("userlastname").value;
   const dob = document.getElementById("userdob").value;
   addUser(name, lastname, dob);
 };
+
