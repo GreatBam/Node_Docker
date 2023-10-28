1307e7284dadaba398293c7e8a795fa15c838b15
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-28 08:42:34 +0200

    Check user data

--- node/app.js
+++ node/app.js
@@ -104,6 +104,10 @@ app.post("/api/adduser", async (req, res) => {
 app.post("/api/signin", async (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+  
 });
 
 app.listen(port, () => {
