ae153f8b27dd3a2bbbc8415a0f8e61d5404c7231
Jonathan <jonathan.gabioud@hotmail.com>
2023-10-28 11:35:53 +0200

    Remove script logs

--- html/js/script.js
+++ html/js/script.js
@@ -1,18 +1,16 @@
-const signinToggler = document.getElementById('signin-toggler');
-const signupToggler = document.getElementById('signup-toggler');
-const sigininContainer = document.querySelector('.signin-container');
-const siginupContainer = document.querySelector('.signup-container');
+const signinToggler = document.getElementById("signin-toggler");
+const signupToggler = document.getElementById("signup-toggler");
+const sigininContainer = document.querySelector(".signin-container");
+const siginupContainer = document.querySelector(".signup-container");
 
-siginupContainer.style.display = 'none';
+siginupContainer.style.display = "none";
 
 signinToggler.onclick = () => {
-  console.log('clicked in');
-    sigininContainer.style.display = 'block';
-    siginupContainer.style.display = 'none';
-}
+  sigininContainer.style.display = "block";
+  siginupContainer.style.display = "none";
+};
 
 signupToggler.onclick = () => {
-  console.log('clicked up');
-    sigininContainer.style.display = 'none';
-    siginupContainer.style.display = 'block';
-}
+  sigininContainer.style.display = "none";
+  siginupContainer.style.display = "block";
+};
